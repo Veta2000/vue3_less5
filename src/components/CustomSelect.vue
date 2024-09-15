179723de8802613ae4ab7f1c3a316ab6016eaf69
Veta2000 <elizter121@gmail.com>
2024-09-15 21:49:19 +0300

    Компонент с выпадающим списком

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,19 @@
+<template>
+    <select v-model="selected">
+      <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
+    </select>
+  </template>
+  
+  <script setup>
+  import { ref, watch } from 'vue';
+  
+  const props = defineProps(['modelValue', 'options']);
+  const emit = defineEmits(['update:modelValue']);
+  
+  const selected = ref(props.modelValue);
+  
+  watch(selected, (newValue) => {
+    emit('update:modelValue', newValue);
+  });
+  </script>
+  
